27b21d24e8c1260f2bcb8069cc4a1b98c0bc4082
pahntanapat <pahntanapat@gmail.com>
2022-06-05 13:02:28 +0700

    add test sample

---
+++ test/test.py
@@ -0,0 +1,23 @@
+
+from math import exp
+import numpy as np
+from os import cpu_count
+from time import sleep, perf_counter
+
+# Simulate parallel processing with sleep >> Capture time and compare
+# Series sleep
+for loop in range(cpu_count()):
+    sleep(1)
+
+
+for loop in range(4*cpu_count()):
+    sleep(1)
+# StackTracedThreadPoolExecutor sleep
+# StackTracedProcessPoolExecutor sleep
+# PipeProcessPoolExecutor sleep
+
+# Simulate parallel processing with GIL-released process
+np.exp(np.arange(100000))
+
+# Simulate parallel processing with GIL process
+[exp(i) for i in range(100000)]
