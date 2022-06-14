f25ffc1197d25b8f9577711f41a21dba155c5d9c
pahntanapat <pahntanapat@gmail.com>
2022-06-14 07:05:09 +0700

    move file

--- test/test.py
+++ test/test.py
@@ -1,34 +1,103 @@
+from concurrent.futures import ALL_COMPLETED, wait
+from multiprocessing import cpu_count, freeze_support
+from time import sleep, perf_counter
+import time
 from math import log
 import numpy as np
-from os import cpu_count
-from time import sleep, perf_counter
-
-# Simulate parallel processing with sleep >> Capture time and compare
-sleep(1)
-# Series sleep
-for loop in range(cpu_count()):
-    sleep(1)
+import sys
+import os
 
 
-for loop in range(4*cpu_count()):
-    sleep(1)
-# StackTracedThreadPoolExecutor sleep
-# StackTracedProcessPoolExecutor sleep
-# PipeProcessPoolExecutor sleep
+sys.path.insert(0, os.path.join(os.path.dirname(__file__), '..', 'src'))
+from KnkPoolExecutor import PipeProcessPoolExecutor, StackTracedThreadPoolExecutor, StackTracedProcessPoolExecutor
+LOOP = cpu_count()
+if __name__ == '__main__':
+    freeze_support()
 
-# Simulate parallel processing with GIL-released process
-np.log(np.arange(1, 100000))
+    print('Baseline')
 
-for loop in range(cpu_count()):
-    # print(loop)
-    np.log(np.arange(1, 100000))
 
-# Simulate parallel processing with GIL process
-for i in range(1, 100000):
-    # print(i)
-    log(i)
 
-# Series
-for loop in range(cpu_count()):
-    for i in range(1, 100000):
-        log(i)
+    with StackTracedThreadPoolExecutor() as pool:
+        t1str = perf_counter()
+        r = [pool.submit(sleep, 1) for _ in range(LOOP)]
+        wait(r, return_when=ALL_COMPLETED)
+        for i in r:
+            print(i.result())
+        t1stp = perf_counter()
+        print("In StackTracedThreadPoolExecutor sleep : --- %s seconds ---" %
+              (t1stp - t1str))
+
+        t2str = perf_counter()
+        wnp = [pool.submit(np.log, i) for i in range(1, 1+LOOP)]
+        wait(wnp, return_when=ALL_COMPLETED)
+        for i in wnp:
+            print(i.result())
+        t2stp = perf_counter()
+        print("In StackTracedThreadPoolExecutor np : --- %s seconds ---" %
+              (t2stp - t2str))
+
+        t3str = perf_counter()
+        gil = [pool.submit(log, i) for i in range(1, 1+LOOP)]
+        wait(gil, return_when=ALL_COMPLETED)
+        for i in gil:
+            print(i.result())
+        t3stp = perf_counter()
+        print("In StackTracedThreadPoolExecutor gil : --- %s seconds ---" %
+              (t3stp - t3str))
+
+    with StackTracedProcessPoolExecutor() as pool:
+        r = [pool.submit(sleep, 1) for _ in range(LOOP)]
+        wait(r, return_when=ALL_COMPLETED)
+        t1str = perf_counter()
+        for i in r:
+            print(i.result())
+        t1stp = perf_counter()
+        print("In StackTracedProcessPoolExecutor sleep : --- %s seconds ---" %
+              (t1stp - t1str))
+
+        wnp = [pool.submit(np.log, i) for i in range(1, 1+LOOP)]
+        wait(wnp, return_when=ALL_COMPLETED)
+        t2str = perf_counter()
+        for i in wnp:
+            print(i.result())
+        t2stp = perf_counter()
+        print("In StackTracedProcessPoolExecutor np : --- %s seconds ---" %
+              (t2stp - t2str))
+
+        gil = [pool.submit(log, i) for i in range(1, 1+LOOP)]
+        wait(gil, return_when=ALL_COMPLETED)
+        t3str = perf_counter()
+        for i in gil:
+            print(i.result())
+        t3stp = perf_counter()
+        print("In StackTracedProcessPoolExecutor gil : --- %s seconds ---" %
+              (t3stp - t3str))
+
+    with PipeProcessPoolExecutor() as pool:
+        r = [pool.submit(sleep, 1) for _ in range(LOOP)]
+        wait(r, return_when=ALL_COMPLETED)
+        t1str = perf_counter()
+        for i in r:
+            print(i.result())
+        t1stp = perf_counter()
+        print("In PipeProcessPoolExecutor sleep : --- %s seconds ---" %
+              (t1stp - t1str))
+
+        wnp = [pool.submit(np.log, i) for i in range(1, 1+LOOP)]
+        wait(wnp, return_when=ALL_COMPLETED)
+        t2str = perf_counter()
+        for i in wnp:
+            print(i.result())
+        t2stp = perf_counter()
+        print("In PipeProcessPoolExecutor np : --- %s seconds ---" %
+              (t2stp - t2str))
+
+        gil = [pool.submit(log, i) for i in range(1, 1+LOOP)]
+        wait(gil, return_when=ALL_COMPLETED)
+        t3str = perf_counter()
+        for i in gil:
+            print(i.result())
+        t3stp = perf_counter()
+        print("In PipeProcessPoolExecutor gil : --- %s seconds ---" %
+              (t3stp - t3str))
