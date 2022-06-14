6172d9fbd28015a7cfe7b50abba9c0894cc21915
pahntanapat <pahntanapat@gmail.com>
2022-06-14 09:47:22 +0700

    edit utils

--- src/KnkPoolExecutor/utils.py
+++ src/KnkPoolExecutor/utils.py
@@ -1,17 +1,19 @@
 from time import perf_counter
-from typing import Optional
+from traceback import print_exception
 
 
 class CacheProcessTime:
-    start: Optional[float] = None
-    stop: Optional[float] = None
-    duration: Optional[float] = None
+    start: float = None
+    stop: float = None
+    duration: float = None
 
     def __enter__(self):
         self.stop = None
         self.duration = None
         self.start = perf_counter()
 
-    def __exit__(self):
+    def __exit__(self, exc_type, exc_value, tb):
         self.stop = perf_counter()
         self.duration = self.stop - self.start
+        if exc_type is not None:
+            print_exception(exc_type, exc_value, tb)
